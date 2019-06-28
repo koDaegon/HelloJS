5dc43ff3975e311ba05218ad4f87767df21c4c0e
koDaegon <daegon7897@gmail.com>
2019-06-28 17:03:01 -0400

    update clock and greetings

--- greeting.js
+++ greeting.js
@@ -1,8 +1,28 @@
 const form = document.querySelector(".js_form") , 
-	input = form.querySelector("input");
+	input = form.querySelector("input"),
+	greeting = document.querySelector(".js-greetings");
 
-const  USER_LS = "currentUser",
-	SHOWING_CN = "showing";
+const  	USER_LS = "currentUser",
+		SHOWING_CN = "showing";
+
+function askForName() {
+	form.classList.add(SHOWING_CN);
+	form.addEventListener("submit" , handleSubmit);
+}
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const currentValue = input.value;
+	paintGreeting(currentValue);
+	saveName(currentValue);
+}
+
+
+function paintGreeting(text) {
+	form.classList.remove(SHOWING_CN);
+	greeting.classList.add(SHOWING_CN);
+	greeting.innerText = `Hello ${text}`;
+}
 
 function saveName(text){
 	localStorage.setItem(USER_LS , text);
@@ -10,7 +30,11 @@ function saveName(text){
 
 function loadName() {
 	const currentUser = localStorage.getItem(USER_LS);
-	input.innerText = currentUser;
+	if(currentUser === null) {
+		askForName();
+	} else {
+		paintGreeting(currentUser);
+	}
 }
 
 function init() {
