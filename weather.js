34136bee37483c3367e39650e16933be7b2d59c2
koDaegon <daegon7897@gmail.com>
2019-07-25 16:35:32 -0400

    Update for weather API

--- weather.js
+++ weather.js
@@ -1,8 +1,28 @@
 const COORDS = 'coords';
+const API_KEY ="07935da8414bb5db2bf21c0cdcbd9e4b";
+const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
+const weather = document.querySelector(".js-weather");
+
 
 function askLocation() {
     navigator.geolocation.getCurrentPosition(handleGeoSuccess , handleGeoError);
 }
+
+function getWeather(lat , lon) {
+    fetch (`${WEATHER_API}lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json()
+    })
+    .then(function(json) {
+        const temp = json.main.temp;
+        const max_temp = json.main.temp_max;
+        const min_temp = json.main.temp_min;
+        const location = json.name;
+        weather.innerText = (`${location} @ ${temp}°C  `);
+        weather.innerHTML +=`<div>${max_temp}°C / ${min_temp}°C</div>`;
+        console.log(json);
+    });
+    }
 
 function handleGeoError() {
     alert("Loading Location failed");
@@ -21,6 +41,7 @@ function handleGeoSuccess(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 
 function loadCoords() {
@@ -29,6 +50,9 @@ function loadCoords() {
         askLocation();
     } else {
         //getWeather
+        const parseCoords = JSON.parse(loadCoords);
+        
+        getWeather(parseCoords.latitude , parseCoords.longitude);
     }
 }
 
